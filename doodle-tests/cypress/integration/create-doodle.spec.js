2a1f0527e549d3d407f4faf8d93aa39daaaa3126
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-12-29 18:39:04 +0100

    Add additional test for create doodle journey Fix selectors for the last step of the journey

--- doodle-tests/cypress/integration/create-doodle.spec.js
+++ doodle-tests/cypress/integration/create-doodle.spec.js
@@ -30,6 +30,22 @@ describe('doodle tests', () => {
     it('should select an option to continue', () => {
         cy.get(selectors.doodleStepTwoDateSelection).click()
         cy.get(selectors.doodleStepTwoContinueButton).click()
-        cy.url().should('contain', '/create/options')
+        cy.url().should('contain', '/create/settings')
+    }) 
+
+    it('should select poll settings', () => {
+        cy.get(selectors.doodleStepThreeSecondOption).click()
+        cy.get(selectors.doodleStepThreeSecondOptionVotes).clear().type(2)
+        cy.get(selectors.doodleStepThreeThrirdOption).click()
+        cy.get(selectors.doodleStepThreeFourthOption).click()
+        cy.get(selectors.doodleStepThreeContinueButton).click()
+        cy.url().should('contain', '/create/initiator')
+    })
+
+    it('should finish the poll', () => {
+        cy.get(selectors.doodleStepFourName).type('Hi, my name is, chk-chk-chk Slim Shaddy')
+        cy.get(selectors.doodleStepFourEmail).type('mirza.test@getnada.com')
+        cy.get(selectors.doodleStepFourFinishButton).click()
+        cy.url().should('contain', '/poll/')
     })
 })
--- doodle-tests/cypress/support/selectors.js
+++ doodle-tests/cypress/support/selectors.js
@@ -2,8 +2,17 @@
 export default {
     createDoodleButton: '[data-testid="navigation-menu-create-date-poll"]',
     doodleStepOneContinueButton: '.d-actionButtons > .d-button',
-    doodleStepOneContinueButton: '.d-actionButtons > .d-button',
     doodleStepOneTitleInput: '#d-pollTitleContainer > .d-fieldContainer > .d-inputContainer',
     doodleStepTwoDateSelection: '[data-time="02:30:00"] > :nth-child(2)',
-    doodleStepTwoContinueButton: '#d-wizardOptionsNavigationView > .d-stickyContainer > .d-stickyElement > .d-actionButtons > .d-nextButton'
+    doodleStepTwoContinueButton: '#d-wizardOptionsNavigationView > .d-stickyContainer > .d-stickyElement > .d-actionButtons > .d-nextButton',
+    doodleStepThreeFirstOption: '#d-ifneedbeCheckboxContainer > .d-content > .d-action > label > .d-checkmarkWrapper > .d-checkmark > .d-defaultCheckmark > .d-background',
+    doodleStepThreeSecondOption: '#d-columnConstraintCheckboxContainer > .d-content > .d-action > label > .d-checkmarkWrapper > .d-checkmark > .d-defaultCheckmark > .d-background',
+    doodleStepThreeSecondOptionVotes: '#d-columnConstraintInput',
+    doodleStepThreeThrirdOption: '#d-rowConstraintCheckboxContainer > .d-content > .d-action > label > .d-checkmarkWrapper > .d-checkmark > .d-defaultCheckmark > .d-background',
+    doodleStepThreeFourthOption: '#d-hiddenCheckboxContainer > .d-content > .d-action > label > .d-checkmarkWrapper > .d-checkmark > .d-defaultCheckmark > .d-background',
+    doodleStepThreeContinueButton: '#d-wizardSettingsNavigationView > .d-stickyContainer > .d-stickyElement > .d-actionButtons > .d-nextButton',
+    doodleStepFourName: '#d-initiatorNameInput > .d-fieldContainer > .d-inputContainer',
+    doodleStepFourEmail: '#d-initiatorEmailInput > .d-fieldContainer > .d-inputContainer',
+    doodleStepFourFinishButton: '#d-persistPollButton > .d-buttonContainer > .d-buttonContent > .d-textContainer > .d-text'
   };
+
